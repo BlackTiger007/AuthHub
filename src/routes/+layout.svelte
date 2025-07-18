3b00629d98d0b20442e4ef5b70d7eddcc6601e04
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-07-18 09:46:49 +0200

    feat(layout): add canonical link to improve SEO and page structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<svelte:head>
+	<link rel="canonical" href="{page.url.origin}{page.url.pathname}" />
+</svelte:head>
+
+<div class="bg-base-300 flex min-h-screen flex-col">
+	{@render children?.()}
+</div>
