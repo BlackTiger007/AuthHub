d10a8636cc1f028a8d3fe689ff77d4f2cca6c78a
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-07-18 09:46:07 +0200

    feat(events): add LogEvent enum for user and setting related events

---
+++ src/lib/utils/events.ts
@@ -0,0 +1,10 @@
+export enum LogEvent {
+	UserCreated = 'UserCreated',
+	UserDeleted = 'UserDeleted',
+	UserUpdated = 'UserUpdated',
+	UserVerified = 'UserVerified',
+	UserUnverified = 'UserUnverified',
+	UserLogin = 'UserLogin',
+	UserLogout = 'UserLogout',
+	SettingUpdated = 'SettingUpdated'
+}
