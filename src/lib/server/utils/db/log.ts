c685f248bd9a4434ba0aa0e6e18d9572ab8599e5
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-07-18 09:45:39 +0200

    feat(log): implement writeLog function for logging events with user and request details

---
+++ src/lib/server/utils/db/log.ts
@@ -0,0 +1,18 @@
+import { db } from '$lib/server/db';
+import { log } from '$lib/server/db/schema/log';
+import type { RequestEvent } from '@sveltejs/kit';
+import type { LogEvent } from '$lib/utils/events';
+import { generateId } from '../auth';
+
+export async function writeLog(event: RequestEvent, logEvent: LogEvent, data?: unknown) {
+	await db.insert(log).values({
+		id: generateId(),
+		event: logEvent,
+		userId: event.locals.user?.id,
+		ip: event.getClientAddress(),
+		userAgent: event.request.headers.get('user-agent') ?? '',
+		referer: event.request.headers.get('referer') ?? '',
+		data: data ? JSON.stringify(data) : null,
+		createdAt: new Date()
+	});
+}
