cecad7aab520351f2f6b094cbeb6f848e6e0976a
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-07-18 16:52:02 +0200

    feat(layout): add layout component with sidebar and mobile navigation

---
+++ src/routes/(admin)/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Navbar from '$lib/components/navbar.svelte';
+
+	let { children } = $props();
+</script>
+
+<div class="drawer lg:drawer-open">
+	<input id="drawer-toggle" type="checkbox" class="drawer-toggle peer" />
+
+	<div class="drawer-content flex flex-col peer-checked:overflow-hidden">
+		<!-- Mobile Button -->
+		<div class="p-4 lg:hidden">
+			<label for="drawer-toggle" class="btn btn-primary">☰ Menü</label>
+		</div>
+
+		<!-- Seiteninhalt -->
+		<div class="bg-base-300 flex min-h-screen flex-col">
+			{@render children?.()}
+		</div>
+	</div>
+
+	<!-- Sidebar -->
+	<div class="drawer-side">
+		<label for="drawer-toggle" class="drawer-overlay"></label>
+		<Navbar />
+	</div>
+</div>
