4fc522ad0ec461860713b63c5c30058251101d86
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-07-18 09:44:37 +0200

    feat(error): add error handling page with status and message display

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<main class="flex grow items-center justify-center">
+	<div class="max-w-md text-center">
+		<h1>{page.status}</h1>
+		<p class=" py-6 text-xl">{page.error?.message}</p>
+		<a href="/" class="btn btn-info">Startseite</a>
+	</div>
+</main>
