b445b953fa2ef0a2051d7776d823740bbc9108af
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-07-19 15:55:12 +0200

    feat(setting): change value field to store JSON data

--- src/lib/server/db/schema/setting.ts
+++ src/lib/server/db/schema/setting.ts
@@ -2,7 +2,7 @@ import { sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const setting = sqliteTable('setting', {
 	key: text('key').primaryKey(),
-	value: text('value').notNull()
+	value: text('value', { mode: 'json' }).notNull()
 });
 
 export type Setting = typeof setting.$inferSelect;
