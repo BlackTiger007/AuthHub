6e4a584bb0e6adbb8efef86f3a9f8578729fb355
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-07-19 15:55:20 +0200

    feat(settings): update Discord and GitHub schemas to use clientID and clientSecret, and enhance Password schema

--- src/lib/server/utils/settingsSchemas.ts
+++ src/lib/server/utils/settingsSchemas.ts
@@ -1,32 +1,8 @@
 import { z } from 'zod';
-
-export const Discord = z.object({
-	setup: z.boolean().default(false),
-	'Client ID': z.string().default(''),
-	'Client Secret': z.string().default('')
-});
-
-export const GitHub = z.object({
-	setup: z.boolean().default(false),
-	'Client ID': z.string().default(''),
-	'Client Secret': z.string().default('')
-});
-
-export const SMTP = z.object({
-	setup: z.boolean().default(false),
-	host: z.string().default(''),
-	port: z.number().default(587),
-	username: z.string().default(''),
-	password: z.string().default(''),
-	from: z.string().default(''),
-	to: z.string().default(''),
-	subject: z.string().default(''),
-	text: z.string().default(''),
-	html: z.string().default('')
-});
+import { discordScopes, githubScopes } from './scopes';
 
 export const Password = z.object({
-	length: z.number().default(8),
+	length: z.number().min(0).default(8),
 	uppercase: z.boolean().default(true),
 	lowercase: z.boolean().default(true),
 	numbers: z.boolean().default(true),
@@ -34,9 +10,29 @@ export const Password = z.object({
 	exclude: z.string().default('')
 });
 
-export const SMS = z.object({
+export const Discord = z.object({
 	setup: z.boolean().default(false),
-	provider: z.string().default(''),
-	apiKey: z.string().default(''),
-	senderId: z.string().default('')
+	scopes: z.array(z.enum(discordScopes)).default(['identify', 'email']),
+	clientID: z.string().default(''),
+	clientSecret: z.string().default('')
+});
+
+export const GitHub = z.object({
+	setup: z.boolean().default(false),
+	scopes: z.array(z.enum(githubScopes)).default(['user:email']),
+	clientID: z.string().default(''),
+	clientSecret: z.string().default('')
+});
+
+export const SMTP = z.object({
+	setup: z.boolean().default(false),
+	host: z.string().default(''),
+	port: z.number().min(0).default(587),
+	username: z.string().default(''),
+	password: z.string().default(''),
+	from: z.string().default(''),
+	to: z.string().default(''),
+	subject: z.string().default(''),
+	text: z.string().default(''),
+	html: z.string().default('')
 });
