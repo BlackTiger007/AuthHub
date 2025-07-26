e4e92b878f80963d8f7aa73067dabbd912c5e6c6
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-07-26 19:24:28 +0200

    feat(2fa): add recovery code retrieval with proper 2FA and session validation

---
+++ src/routes/recovery-code/+page.server.ts
@@ -0,0 +1,24 @@
+import { getUserRecoverCode } from '$lib/server/utils/user';
+import { redirect } from '@sveltejs/kit';
+import { get2FARedirect } from '$lib/server/utils/2fa';
+
+import type { RequestEvent } from './$types';
+
+export async function load(event: RequestEvent) {
+	if (event.locals.session === null || event.locals.user === null) {
+		return redirect(302, '/login');
+	}
+	if (!event.locals.user.emailVerified) {
+		return redirect(302, '/verify-email');
+	}
+	if (!event.locals.user.registered2FA) {
+		return redirect(302, '/2fa/setup');
+	}
+	if (!event.locals.session.twoFactorVerified) {
+		return redirect(302, get2FARedirect(event.locals.user));
+	}
+	const recoveryCode = await getUserRecoverCode(event.locals.user.id);
+	return {
+		recoveryCode
+	};
+}
