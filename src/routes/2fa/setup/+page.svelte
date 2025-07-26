0becd475cdf3c6ee2f6b5498507237275d544c53
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-07-26 19:20:50 +0200

    feat(2fa/setup): add 2FA setup options page with links to TOTP, Passkeys, and Security Keys

---
+++ src/routes/2fa/setup/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Set up two-factor authentication</h1>
+<ul>
+	<li><a href="/2fa/totp/setup" class="link">Authenticator apps</a></li>
+	<li><a href="/2fa/passkey/register" class="link">Passkeys</a></li>
+	<li><a href="/2fa/security-key/register" class="link">Security keys</a></li>
+</ul>
