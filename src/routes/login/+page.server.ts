6a3b711a3ec32e4cd7064f25eb2a3f4cbf43597f
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-07-27 12:14:57 +0200

    refactor(auth): streamline login logic and add rate limiting

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,17 +1,29 @@
-import { verify } from '@node-rs/argon2';
 import { fail, redirect } from '@sveltejs/kit';
-import { eq } from 'drizzle-orm';
-import * as auth from '$lib/server/auth';
-import { db } from '$lib/server/db';
-import type { Actions, PageServerLoad } from './$types';
-import { validateEmail, validatePassword, validateUsername } from '$lib/server/utils/auth';
-import { writeLog } from '$lib/server/utils/db/log';
-import { LogEvent } from '$lib/utils/events';
-import { schema } from '$lib/server/db/schema';
+import { verifyEmailInput } from '$lib/server/utils/email';
+import { getUserFromEmail, getUserPasswordHash } from '$lib/server/utils/user';
+import { RefillingTokenBucket, Throttler } from '$lib/server/utils/rate-limit';
+import { verifyPasswordHash } from '$lib/server/utils/password';
+import {
+	createSession,
+	generateSessionToken,
+	setSessionTokenCookie
+} from '$lib/server/utils/session';
+import { get2FARedirect } from '$lib/server/utils/2fa';
+
+import type { Actions, PageServerLoad, RequestEvent } from './$types';
 import { settings } from '$lib/server/store.svelte';
 
 export const load: PageServerLoad = async (event) => {
 	if (event.locals.session !== null && event.locals.user !== null) {
+		if (!event.locals.user.emailVerified) {
+			return redirect(302, '/verify-email');
+		}
+		if (!event.locals.user.registered2FA) {
+			return redirect(302, '/2fa/setup');
+		}
+		if (!event.locals.session.twoFactorVerified) {
+			return redirect(302, get2FARedirect(event.locals.user));
+		}
 		return redirect(302, '/');
 	}
 
@@ -25,63 +37,81 @@ export const load: PageServerLoad = async (event) => {
 	return { discord, github };
 };
 
+const throttler = new Throttler<string>([0, 1, 2, 4, 8, 16, 30, 60, 180, 300]);
+const ipBucket = new RefillingTokenBucket<string>(20, 1);
+
 export const actions: Actions = {
-	login: async (event) => {
-		const formData = await event.request.formData();
-		const username = formData.get('username');
-		const email = formData.get('email');
-		const password = formData.get('password');
-
-		if (!validateUsername(username)) {
-			return fail(400, {
-				message: 'Invalid username (min 3, max 31 characters, alphanumeric only)'
-			});
-		}
-
-		if (!validateEmail(email)) {
-			return fail(400, { message: 'Invalid email' });
-		}
-
-		if (!validatePassword(password)) {
-			return fail(400, { message: 'Invalid password (min 6, max 255 characters)' });
-		}
-
-		const results = await db.select().from(schema.user).where(eq(schema.user.email, email));
-
-		const existingUser = results.at(0);
-
-		if (!existingUser) {
-			return fail(400, { message: 'Incorrect email or password' });
-		}
-
-		if (existingUser.passwordHash === null) {
-			return fail(400, {
-				message:
-					'This account was created via an external provider (e.g., Discord) and does not have a password. Please sign in using the respective provider.'
-			});
-		}
-
-		const validPassword = await verify(existingUser.passwordHash, password, {
-			memoryCost: 19456,
-			timeCost: 2,
-			outputLen: 32,
-			parallelism: 1
-		});
-		if (!validPassword) {
-			return fail(400, { message: 'Incorrect email or password' });
-		}
-
-		const sessionToken = auth.generateSessionToken();
-		const session = await auth.createSession(sessionToken, existingUser.id);
-		auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
-
-		await db
-			.update(schema.user)
-			.set({ lastLogin: new Date() })
-			.where(eq(schema.user.id, existingUser.id));
-
-		await writeLog(event, LogEvent.UserLogin, { userId: existingUser.id });
-
-		return redirect(302, '/admin');
-	}
+	default: action
 };
+
+async function action(event: RequestEvent) {
+	// TODO: Assumes X-Forwarded-For is always included.
+	const clientIP = event.request.headers.get('X-Forwarded-For');
+	if (clientIP !== null && !ipBucket.check(clientIP, 1)) {
+		return fail(429, {
+			message: 'Too many requests',
+			email: ''
+		});
+	}
+
+	const formData = await event.request.formData();
+	const email = formData.get('email');
+	const password = formData.get('password');
+	if (typeof email !== 'string' || typeof password !== 'string') {
+		return fail(400, {
+			message: 'Invalid or missing fields',
+			email: ''
+		});
+	}
+	if (email === '' || password === '') {
+		return fail(400, {
+			message: 'Please enter your email and password.',
+			email
+		});
+	}
+	if (!verifyEmailInput(email)) {
+		return fail(400, {
+			message: 'Invalid email',
+			email
+		});
+	}
+	const user = await getUserFromEmail(email);
+	if (user === null) {
+		return fail(400, {
+			message: 'Account does not exist',
+			email
+		});
+	}
+	if (clientIP !== null && !ipBucket.consume(clientIP, 1)) {
+		return fail(429, {
+			message: 'Too many requests',
+			email: ''
+		});
+	}
+	if (!throttler.consume(user.id)) {
+		return fail(429, {
+			message: 'Too many requests',
+			email: ''
+		});
+	}
+	const passwordHash = await getUserPasswordHash(user.id);
+	const validPassword = await verifyPasswordHash(passwordHash, password);
+	if (!validPassword) {
+		return fail(400, {
+			message: 'Invalid password',
+			email
+		});
+	}
+	throttler.reset(user.id);
+	const sessionToken = generateSessionToken();
+	const session = await createSession(sessionToken, user.id, false);
+	setSessionTokenCookie(event, sessionToken, session.expiresAt);
+
+	if (!user.emailVerified) {
+		return redirect(302, '/verify-email');
+	}
+	if (!user.registered2FA) {
+		return redirect(302, '/2fa/setup');
+	}
+	return redirect(302, get2FARedirect(user));
+}
