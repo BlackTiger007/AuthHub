e2d3ff6952cff35ce9709c86add5ee89cfa92c16
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-07-27 13:22:52 +0200

    feat(auth): implement 2FA and logout logic in root page server
    - Added full 2FA checks and redirects in `/page.server.ts`
    - Ensures user is verified before accessing protected root
    - Added logout action to invalidate session and delete cookie
    - Now returns `publicUser` for client-side rendering

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,39 @@
-import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
-import { Role } from '$lib/utils/roles';
+import { fail, redirect } from '@sveltejs/kit';
+import { deleteSessionTokenCookie, invalidateSession } from '$lib/server/utils/session';
+import { get2FARedirect } from '$lib/server/utils/2fa';
 
-export const load = (async ({ locals }) => {
-	if (locals.user && locals.user.role > Role.Admin) {
-		return redirect(302, '/admin');
+import type { Actions, PageServerLoadEvent, RequestEvent } from './$types';
+import { publicUser } from '$lib/server/utils/publicCLientUser';
+
+export function load(event: PageServerLoadEvent) {
+	if (event.locals.session === null || event.locals.user === null) {
+		return redirect(302, '/login');
 	}
+	if (!event.locals.user.emailVerified) {
+		return redirect(302, '/verify-email');
+	}
+	if (!event.locals.user.registered2FA) {
+		return redirect(302, '/2fa/setup');
+	}
+	if (!event.locals.session.twoFactorVerified) {
+		return redirect(302, get2FARedirect(event.locals.user));
+	}
+	return {
+		user: publicUser(event.locals.user)
+	};
+}
 
-	return {};
-}) satisfies PageServerLoad;
+export const actions: Actions = {
+	default: action
+};
+
+async function action(event: RequestEvent) {
+	if (event.locals.session === null) {
+		return fail(401, {
+			message: 'Not authenticated'
+		});
+	}
+	invalidateSession(event.locals.session.id);
+	deleteSessionTokenCookie(event);
+	return redirect(302, '/login');
+}
