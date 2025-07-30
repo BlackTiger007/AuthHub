249203642900b2649b303f12d24ab25d6a0ced89
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-07-30 20:24:22 +0200

    fix(admin): redirect unauthenticated users and restrict access by role

--- src/routes/(admin)/+layout.server.ts
+++ src/routes/(admin)/+layout.server.ts
@@ -1,11 +1,13 @@
-import { requireLogin } from '$lib/server/utils/auth';
 import { Role } from '$lib/utils/roles';
-import { error } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
-export const load = (async () => {
-	const user = requireLogin();
-	if (user.role < Role.Admin) {
+export const load = (async ({ locals }) => {
+	if (!locals.user) {
+		return redirect(302, '/login');
+	}
+
+	if (locals.user.role < Role.Admin) {
 		error(403, 'Forbidden');
 	}
 	return {};
